d8886cda697336b9f4a465d7c91d0c9bfb7d18a4
s20161104566 <s20161104566@admindeimac-35.local>
2017-06-16 14:51:17 +0800

    Initial Commit

---
+++ jsj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  jsj
+//
+//  Created by s20161104566 on 2017/6/16.
+//  Copyright © 2017年 s20161104566. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
